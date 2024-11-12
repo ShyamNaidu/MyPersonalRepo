5f3f712369df2b759401f10c39d22003d7ed3c89
ShyamNaidu <syam.svs@gmail.com>
2024-11-12 14:12:41 +0000

    Modified Code on 111224

--- MyReact/src/App.jsx
+++ MyReact/src/App.jsx
@@ -1,16 +1,56 @@
+import { useState } from 'react';
 import './App.css'
+import pen from "./images/pen.jpg"
 
-function Header(){
+function Header({ name , year}){
   return(
     <header>
-      <h1>I am learning React</h1>
+      <h2>I am learning React - {name} - {year}</h2>
     </header>
   )
 }
 
+const items = [
+  "Idly",
+  "Upma",
+  "Dosa"
+]
+
+const dishObjects = items.map((dish,i) => ({
+  id: i,
+  title: dish
+}));
+
+function List({dishes}){
+  return(
+    <main>
+      <img src={pen} alt='My Pen' />
+    <ul>
+      {dishes.map(dish => 
+          (<li key={dish.id} style={{listStyleType: "none"}}>
+              {dish.title}
+            </li>))}
+    </ul>
+    </main>
+  )
+}
+
+
+
+
 function App() {
+  const [status, setStatus] = useState("Open");
+  console.log(status);
+
+
+
   return (
-    <Header />
+  <div>
+    <h1>The school is currently {status}</h1>
+    <button onClick={() => setStatus("Closed")}>Close School</button>
+    <Header name="Alex" year={new Date().getFullYear()} />
+    <List dishes={dishObjects} />
+  </div>
   );
 }
 
